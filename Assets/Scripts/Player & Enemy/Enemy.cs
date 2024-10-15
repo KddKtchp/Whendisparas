aea1a68d641866351c960da7897d80f7f2087b27
KddKtchp <yoanmorilolxd@gmail.com>
2024-10-15 12:59:06 -0300

    1.5
    - probuilder my beloved
    - poner dialogos 8un personaje
    -hacer levels

--- Assets/Scripts/Player & Enemy/Enemy.cs
+++ Assets/Scripts/Player & Enemy/Enemy.cs
@@ -8,7 +8,12 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] GameObject projectilePrefab; // Prefab del proyectil
     [SerializeField] Transform firePoint; // Punto desde el que dispara el proyectil
     [SerializeField] float collisionDamage = 10f; // Daño que hace el enemigo al colisionar con el jugador
-
+    [SerializeField] float shootPause = 1;
+    [SerializeField] float rotationSpeed = 1;
+    [SerializeField] bool shootWhenLookingAtPlayer = true;
+    [SerializeField] float minimalRotationDistance = 1f;
+    float pausedShooting = 0;
+    
     private Transform player;
 
     void Start()
@@ -18,20 +23,36 @@ public class EnemyBehavior : MonoBehaviour
 
     void Update()
     {
+        if (Time.timeScale == 0) return;
         // Moverse hacia el jugador
         if (player)
         {
+             
             float distance = Vector3.Distance(transform.position, player.position);
+            Quaternion playerRotation = Quaternion.LookRotation(player.position - transform.position);
+
+            Quaternion currentRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, playerRotation.eulerAngles.y, 0), rotationSpeed);
+            transform.rotation = currentRotation;
+
             if (distance < attackRange)
             {
-                // Lógica para atacar o disparar
-                Shoot();
+                if (pausedShooting <= 0)
+                {
+                    if((shootWhenLookingAtPlayer == true && Mathf.Abs(transform.rotation.eulerAngles.y - playerRotation.eulerAngles.y) <= minimalRotationDistance ) || shootWhenLookingAtPlayer == false) { 
+                        // Lógica para atacar o disparar
+                        Shoot();
+                    }
+                }
             }
-            else
-            {
+            else { 
                 // Mover hacia el jugador
                 Vector3 direction = (player.position - transform.position).normalized;
                 transform.position += direction * moveSpeed * Time.deltaTime;
+            }
+
+            if (pausedShooting > 0)
+            {
+                pausedShooting -= Time.deltaTime;
             }
         }
     }
@@ -41,6 +62,7 @@ public class EnemyBehavior : MonoBehaviour
         // Instanciar el proyectil
         GameObject go = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
         go.GetComponent<Rigidbody>().AddForce(transform.forward * attackSpeed);
+        pausedShooting = shootPause;
     }
 
     // Detecta colisiones con el jugador
@@ -57,5 +79,10 @@ public class EnemyBehavior : MonoBehaviour
                 Debug.Log("El enemigo ha causado " + collisionDamage + " de daño al jugador.");
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(player != null) Gizmos.DrawLine(transform.position, player.position);
     }
 }
